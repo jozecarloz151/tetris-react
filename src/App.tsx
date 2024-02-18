60e12ede5a2aaf2969dd8578d77382583ed6156f
José Pecho <jpechob@odybank.com.pe>
2024-02-17 22:08:59 -0500

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { TetrisGame } from './components/TetrisGame'
 
 function App () {
   return (
-    <main>
-      <h1 className="title">TETRIS</h1>
-      <TetrisGame />
-    </main>
+    <>
+      <main>
+        <h1 className="title">TETRIS</h1>
+        <TetrisGame />
+      </main>
+      <footer>
+        <small>You can check out the code <a href='https://github.com/jozedev/tetris-react' target='_blank' rel="noreferrer">here</a>.</small>
+      </footer>
+    </>
   )
 }
 
